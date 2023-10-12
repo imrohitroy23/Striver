9065773a3c3d2afd555cf0fd67f3261d0bc644ae
imrohitroy23 <90301368+imrohitroy23@users.noreply.github.com>
2023-10-12 11:45:13 +0530

    commit

---
+++ BST/CheckForBST.java
@@ -0,0 +1,15 @@
+package BST;
+
+public class CheckForBST {
+         boolean isBST(Node root)
+    {
+        // code here.
+        return check(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+    boolean check(Node root,int min,int max){
+        if(root==null) return true;
+        if(root.data<min || root.data>max) return false;
+        return (check(root.left,min,root.data-1)&&check(root.right,root.data+1,max));
+    }
+    
+}
